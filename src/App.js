04f35cbc2d40134465861ee69a9d4020df11b449
pshrimal000 <pshrimal000@gmail.com>
2021-04-24 10:56:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router basename="/code-editor">
+    <Router basename="/code-editorr">
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/web" component={WebEditor} />
